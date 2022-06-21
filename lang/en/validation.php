1a9e3830aad84700d221533d52a5ffdfb511773f
Italo <DarkGhostHunter@Gmail.com>
2022-06-20 23:01:03 -0400

    Removes unused translation line.

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -3,7 +3,6 @@
 return [
     'rut' => 'The :attribute must be a valid RUT.',
     'strict' => 'The :attribute must be a properly formatted RUT.',
-    'equal' => 'The :attribute must be a valid RUT.',
     'exists' => 'The :attribute must be a valid RUT.',
     'unique' => 'The :attribute has already been taken.',
 ];
