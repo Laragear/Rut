21fe1019fa4b9fd6f86815ea06189f321c4683b2
Italo <DarkGhostHunter@Gmail.com>
2022-06-20 23:00:50 -0400

    Adds spanish validation lines.

---
+++ lang/es/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'rut' => 'El :attribute debe ser un RUT válido.',
+    'strict' => 'El :attribute debe ser un RUT con puntos y guión.',
+    'exists' => 'El :attribute debe ser un RUT válido.',
+    'unique' => 'El :attribute ya ha sido tomado.',
+];
